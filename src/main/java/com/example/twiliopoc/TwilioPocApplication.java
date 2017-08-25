a0b1ce20c62bb52931ffcf57a65a5d8aa0b18f32
camora <camora@westfield.com>
2017-08-25 15:40:43 -0700

    POC initial commit shell

---
+++ src/main/java/com/example/twiliopoc/TwilioPocApplication.java
@@ -0,0 +1,12 @@
+package com.example.twiliopoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwilioPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwilioPocApplication.class, args);
+	}
+}
